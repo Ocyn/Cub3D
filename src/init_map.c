2d5f2be317aa16cd6654fbb63adef94140564920
Ocyn <niko.ocyn@gmail.com>
2024-04-07 03:27:46 +0200

    minor updates

--- src/init_map.c
+++ src/init_map.c
@@ -6,7 +6,7 @@
 /*   By: jcuzin <jcuzin@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 20:53:57 by jcuzin            #+#    #+#             */
-/*   Updated: 2024/04/07 02:07:44 by jcuzin           ###   ########.fr       */
+/*   Updated: 2024/04/07 03:26:34 by jcuzin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,8 @@ int	init_map_texture(t_tex *asset, char **map, char *set, size_t *pos)
 		return (err_return(EXIT_FAILURE, "Missing declaration", 3));
 	if (id > (long long)*pos)
 		*pos = id;
+	if (me_find_str_in_tab(0, set, map + id + 1) != -1)
+		return (err_return(EXIT_FAILURE, "Multiple declaration detected", 3));
 	asset->file = ft_strdup(map[id] + ft_strlen(set));
 	if (!asset->file)
 		return (err_return(EXIT_FAILURE, "Asset extract failed", 3));
@@ -63,16 +65,16 @@ int	init_map_trim(t_map *map, char **temp)
 	if (!map || !temp)
 		return (err_return(EXIT_FAILURE, "Memory issue", 2));
 	if (init_map_texture(&map->tex_no, temp, "NO ", &trim))
-		return (err_return(EXIT_FAILURE, "NO missing", 2));
+		return (err_return(EXIT_FAILURE, "North texture missing", 2));
 	if (init_map_texture(&map->tex_so, temp, "SO ", &trim))
-		return (err_return(EXIT_FAILURE, "SO missing", 2));
+		return (err_return(EXIT_FAILURE, "South texture missing", 2));
 	if (init_map_texture(&map->tex_ea, temp, "EA ", &trim))
-		return (err_return(EXIT_FAILURE, "EA missing", 2));
+		return (err_return(EXIT_FAILURE, "East texture missing", 2));
 	if (init_map_texture(&map->tex_we, temp, "WE ", &trim))
-		return (err_return(EXIT_FAILURE, "WE missing", 2));
+		return (err_return(EXIT_FAILURE, "West texture missing", 2));
 	if (init_map_texture_bis(map->floor, temp, "F ", &trim) \
 	|| init_map_texture_bis(map->roof, temp, "C ", &trim))
-		return (err_return(EXIT_FAILURE, "F / C missing", 2));
+		return (err_return(EXIT_FAILURE, "F / C not defined", 2));
 	trim += (trim > 0);
 	while (!ft_strlen(temp[trim]))
 		trim++;
