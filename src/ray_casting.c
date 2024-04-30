afb3bb22403904599530f8f5ed32dd6b73c5d6dd
Ocyn <niko.ocyn@gmail.com>
2024-04-30 04:01:17 +0200

    minors improvements

--- src/ray_casting.c
+++ src/ray_casting.c
@@ -6,7 +6,7 @@
 /*   By: jcuzin <jcuzin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 07:39:45 by jcuzin            #+#    #+#             */
-/*   Updated: 2024/04/30 03:55:23 by jcuzin           ###   ########.fr       */
+/*   Updated: 2024/04/30 03:58:43 by jcuzin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	rc_minimap_ray(t_data *dt, t_mlx *mlx, int pos[2])
 	y = -46;
 	while (++y <= 45)
 	{
-		ray = mlx->gscale / 4;
+		ray = 10 / mlx->gscale;
 		math_coeff_circle(ray, dt->player.angle + y, cf);
 		while (pos[0] / 2 + cf[0] - RPS > 0 && pos[1] / 2 + cf[1] - RPS > 0 \
 		&& pos[0] / 2 + cf[0] + 1 < pos[0] && pos[1] / 2 + cf[1] + 1 < pos[1] \
